4742c003db04da6703ffe9df340cf3307014ddb9
Jose <xandin.blizz@gmail.com>
2024-07-19 20:48:56 -0300

    Versão 1

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('primeiro projeto')
+
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+
+    total = pizza + refrigerante
+
+    print(total)
+
+calcular_preco_combo(30, 20)
+
+print('Programa finalizado')
