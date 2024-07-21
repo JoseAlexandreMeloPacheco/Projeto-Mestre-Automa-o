cea68107098a77beef4007cace2eaa9ffb1210e5
Jose <xandin.blizz@gmail.com>
2024-07-21 00:39:32 -0300

    App

---
+++ aula2.py
@@ -0,0 +1,7 @@
+
+
+import pyautogui
+
+pyautogui.click(1513, 921, button='right', duration=2)
+pyautogui.click(1340, 880, button='left', duration=2)
+pyautogui.click(1568, 589, button='left', duration=2)
---
+++ aula3.py
@@ -0,0 +1,6 @@
+import pyautogui
+
+for i in range(20):
+    pyautogui.moveTo(1265,300,duration=0.5)
+    pyautogui.dragTo(1421,868,duration=0.5)
+
